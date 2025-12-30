b02a55df867741c837562a671f5212e8ddb32852
Chocolate <hellderick69@gmail.com>
2025-12-30 14:27:48 -0800

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -19,7 +19,8 @@
             { type: "discord", icon: "fab fa-discord", url: "https://discord.com/users/385931079606992911" },
           { type: "lastfm", icon: "fab fa-lastfm", url: "https://last.fm/user/i1h" },
           { type: "reddit", icon: "fab fa-reddit", url: "https://reddit.com/u/50uu" },
-        { type: "roblox", icon: "fas fa-gamepad", url: "https://www.roblox.com/pt/users/193415707/profile" }
+        { type: "roblox", icon: "fas fa-gamepad", url: "https://www.roblox.com/pt/users/193415707/profile" },
+            { type: "vndb", icon: "fas fa-V", url: "https://vndb.org/u328201"}
          ]
       }
     ];
